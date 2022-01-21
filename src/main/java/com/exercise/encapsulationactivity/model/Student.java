38ea2d8de07c01855995ba5b87efe5838e9bd1c2
Gurpreet1490 <g.preet08092014@gmail.com>
2022-01-21 14:28:56 +0000

    Made Changes as per Kim suggestion (if currentGarde=A, then it can't upgrade.

--- src/main/java/com/exercise/encapsulationactivity/model/Student.java
+++ src/main/java/com/exercise/encapsulationactivity/model/Student.java
@@ -3,21 +3,22 @@ package com.exercise.encapsulationactivity.model;
 public class Student {
 
     private final String name;
-    private Grade grade;
+    private static Grade grade;
     private Group group;
-    private final String nickName = "MySecretNickName";
+    private final String nickName = "Nickname";
 
-    public Student(String name, Grade grade, int group){
+    public Student(String name, Grade grade, int group) {
         this.name = name;
         this.grade = grade;
         this.group = Group.getByValue(group);
     }
 
+
     public String getName() {
         return name;
     }
 
-    public Grade getGrade() {
+    public static Grade getGrade() {
         return grade;
     }
 
@@ -25,18 +26,33 @@ public class Student {
         return group.getValue();
     }
 
-    public void upGrade() {
-        Grade newGrade = Grade.getByOrder(grade.getOrder() + 1);
-        if(newGrade != null)    {
+    public Grade upGrade() {
+        Grade newGrade = Grade.getByOrder(grade.getOrder() - 1);
+        Grade currentGrade = getGrade();
+        if (newGrade != Grade.A) {
+            System.out.println("Can't Upgrade Grade.");
+            newGrade = currentGrade;
+            System.out.println(newGrade);
+        } else if (newGrade != null) {
             grade = newGrade;
+            System.out.println(grade);
         }
+        return newGrade;
     }
 
-    public void downGrade() {
-        Grade newGrade = Grade.getByOrder(grade.getOrder() - 1);
-        if(newGrade != null)    {
+    public Grade downGrade() {
+        Grade newGrade = Grade.getByOrder(grade.getOrder() + 1);
+        Grade currentGrade = getGrade();
+        if (newGrade != Grade.F) {
+            System.out.println("Can't change Grade.");
+            newGrade = currentGrade;
+            System.out.println(newGrade);
+        } else if (newGrade != null) {
             grade = newGrade;
+            System.out.println(grade);
         }
+        return newGrade;
+
     }
 
 }
